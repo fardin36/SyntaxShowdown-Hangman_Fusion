e3425a31940d93c9cb78ef854e3c480ed73c1ab7
fardin36 <bahauddinfaruqi@gmai.com>
2024-04-25 14:59:52 +0600

    Syntax Showdown : Hangman Fusion project initialized

---
+++ src/quiz/app/Login.java
@@ -0,0 +1,13 @@
+package quiz.app;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+    Login(){
+        setSize(1000, 500);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Login();
+    }
+}
