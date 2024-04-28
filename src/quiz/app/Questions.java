765a2e0e38dcc1e67ccb145d4b79ed1e762095c4
fardin36 <bahauddinfaruqi@gmai.com>
2024-04-28 12:05:24 +0600

    Modified the initial static questions and moved those to Qestions class

---
+++ src/quiz/app/Questions.java
@@ -0,0 +1,85 @@
+package quiz.app;
+
+public class Questions {
+    public static String[][] questions = new String[10][5];
+    public static String[][] answers = new String[10][2];
+    public static String[][] useranswers = new String[10][1];
+
+
+    static {
+        questions[0][0] = "Number of primitive data types in Java are.?";
+        questions[0][1] = "6";
+        questions[0][2] = "7";
+        questions[0][3] = "8";
+        questions[0][4] = "9";
+
+        questions[1][0] = "What is the size of float and double in java.?";
+        questions[1][1] = "32 and 64";
+        questions[1][2] = "32 and 32";
+        questions[1][3] = "64 and 64";
+        questions[1][4] = "64 and 32";
+
+        questions[2][0] = "Automatic type conversion is possible in which of the possible cases?";
+        questions[2][1] = "Byte to int";
+        questions[2][2] = "Int to Long";
+        questions[2][3] = "Long to int";
+        questions[2][4] = "Short to int";
+
+        questions[3][0] = "When an array is passed to a method, what does the method receive?";
+        questions[3][1] = "The reference of the array";
+        questions[3][2] = "A copy of the array";
+        questions[3][3] = "Length of the array";
+        questions[3][4] = "Copy of first element";
+
+        questions[4][0] = "Arrays in java are.?";
+        questions[4][1] = "Object References";
+        questions[4][2] = "Objects";
+        questions[4][3] = "Primitive data type";
+        questions[4][4] = "None";
+
+        questions[5][0] = "When is the object created with new keyword?";
+        questions[5][1] = "At rum time";
+        questions[5][2] = "At compile time";
+        questions[5][3] = "Depends on the code";
+        questions[5][4] = "None";
+
+        questions[6][0] = "Identify the corrected definition of a package.?";
+        questions[6][1] = "A package is a collection of editing tools";
+        questions[6][2] = "A package is a collection of Classes";
+        questions[6][3] = "A package is a collection of Classes and interfaces";
+        questions[6][4] = "A package is a collection of interfaces";
+
+        questions[7][0] = "compareTo() returns";
+        questions[7][1] = "True";
+        questions[7][2] = "False";
+        questions[7][3] = "An int value";
+        questions[7][4] = "None";
+
+        questions[8][0] = "To which of the following does the class string belong to.";
+        questions[8][1] = "java.lang";
+        questions[8][2] = "java.awt";
+        questions[8][3] = "java.applet";
+        questions[8][4] = "java.String";
+
+        questions[9][0] = "Total constructor string class have.?";
+        questions[9][1] = "3";
+        questions[9][2] = "7";
+        questions[9][3] = "13";
+        questions[9][4] = "20";
+
+        answers[0][1] = "8";
+        answers[1][1] = "32 and 64";
+        answers[2][1] = "Int to Long";
+        answers[3][1] = "The reference of the array";
+        answers[4][1] = "Objects";
+        answers[5][1] = "At rum time";
+        answers[6][1] = "A package is a collection of Classes and interfaces";
+        answers[7][1] = "An int value";
+        answers[8][1] = "java.lang";
+        answers[9][1] = "13";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/quiz/app/Quiz.java
+++ src/quiz/app/Quiz.java
@@ -5,12 +5,13 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import quiz.app.HangmanUtils;
+import quiz.app.Questions;
 
 public class Quiz extends JFrame implements ActionListener {
 
-    String[][] questions = new String[10][5];
-    String[][] answers = new String[10][2];
-    String[][] useranswers = new String[10][1];
+//    String[][] questions = new String[10][5];
+//    String[][] answers = new String[10][2];
+//    String[][] useranswers = new String[10][1];
 
     JLabel qno, question;
     JRadioButton opt1, opt2, opt3, opt4;
@@ -35,7 +36,6 @@ public class Quiz extends JFrame implements ActionListener {
 
     Quiz(String name){
         super("Syntax Showdown : Hangman Fusion");
-
         this.name = name;
 
 //        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/quiz.png"));
@@ -59,76 +59,76 @@ public class Quiz extends JFrame implements ActionListener {
         question.setFont(new Font("Tahoma",Font.PLAIN,24));
         add(question);
 
-        questions[0][0] = "Number of primitive data types in Java are.?";
-        questions[0][1] = "6";
-        questions[0][2] = "7";
-        questions[0][3] = "8";
-        questions[0][4] = "9";
-
-        questions[1][0] = "What is the size of float and double in java.?";
-        questions[1][1] = "32 and 64";
-        questions[1][2] = "32 and 32";
-        questions[1][3] = "64 and 64";
-        questions[1][4] = "64 and 32";
-
-        questions[2][0] = "Automatic type conversion is possible in which of the possible cases?";
-        questions[2][1] = "Byte to int";
-        questions[2][2] = "Int to Long";
-        questions[2][3] = "Long to int";
-        questions[2][4] = "Short to int";
-
-        questions[3][0] = "When an array is passed to a method, what does the method receive?";
-        questions[3][1] = "The reference of the array";
-        questions[3][2] = "A copy of the array";
-        questions[3][3] = "Length of the array";
-        questions[3][4] = "Copy of first element";
-
-        questions[4][0] = "Arrays in java are.?";
-        questions[4][1] = "Object References";
-        questions[4][2] = "Objects";
-        questions[4][3] = "Primitive data type";
-        questions[4][4] = "None";
-
-        questions[5][0] = "When is the object created with new keyword?";
-        questions[5][1] = "At rum time";
-        questions[5][2] = "At compile time";
-        questions[5][3] = "Depends on the code";
-        questions[5][4] = "None";
-
-        questions[6][0] = "Identify the corrected definition of a package.?";
-        questions[6][1] = "A package is a collection of editing tools";
-        questions[6][2] = "A package is a collection of Classes";
-        questions[6][3] = "A package is a collection of Classes and interfaces";
-        questions[6][4] = "A package is a collection of interfaces";
-
-        questions[7][0] = "compareTo() returns";
-        questions[7][1] = "True";
-        questions[7][2] = "False";
-        questions[7][3] = "An int value";
-        questions[7][4] = "None";
-
-        questions[8][0] = "To which of the following does the class string belong to.";
-        questions[8][1] = "java.lang";
-        questions[8][2] = "java.awt";
-        questions[8][3] = "java.applet";
-        questions[8][4] = "java.String";
-
-        questions[9][0] = "Total constructor string class have.?";
-        questions[9][1] = "3";
-        questions[9][2] = "7";
-        questions[9][3] = "13";
-        questions[9][4] = "20";
-
-        answers[0][1] = "8";
-        answers[1][1] = "32 and 64";
-        answers[2][1] = "Int to Long";
-        answers[3][1] = "The reference of the array";
-        answers[4][1] = "Objects";
-        answers[5][1] = "At rum time";
-        answers[6][1] = "A package is a collection of Classes and interfaces";
-        answers[7][1] = "An int value";
-        answers[8][1] = "java.lang";
-        answers[9][1] = "13";
+//        questions[0][0] = "Number of primitive data types in Java are.?";
+//        questions[0][1] = "6";
+//        questions[0][2] = "7";
+//        questions[0][3] = "8";
+//        questions[0][4] = "9";
+//
+//        questions[1][0] = "What is the size of float and double in java.?";
+//        questions[1][1] = "32 and 64";
+//        questions[1][2] = "32 and 32";
+//        questions[1][3] = "64 and 64";
+//        questions[1][4] = "64 and 32";
+//
+//        questions[2][0] = "Automatic type conversion is possible in which of the possible cases?";
+//        questions[2][1] = "Byte to int";
+//        questions[2][2] = "Int to Long";
+//        questions[2][3] = "Long to int";
+//        questions[2][4] = "Short to int";
+//
+//        questions[3][0] = "When an array is passed to a method, what does the method receive?";
+//        questions[3][1] = "The reference of the array";
+//        questions[3][2] = "A copy of the array";
+//        questions[3][3] = "Length of the array";
+//        questions[3][4] = "Copy of first element";
+//
+//        questions[4][0] = "Arrays in java are.?";
+//        questions[4][1] = "Object References";
+//        questions[4][2] = "Objects";
+//        questions[4][3] = "Primitive data type";
+//        questions[4][4] = "None";
+//
+//        questions[5][0] = "When is the object created with new keyword?";
+//        questions[5][1] = "At rum time";
+//        questions[5][2] = "At compile time";
+//        questions[5][3] = "Depends on the code";
+//        questions[5][4] = "None";
+//
+//        questions[6][0] = "Identify the corrected definition of a package.?";
+//        questions[6][1] = "A package is a collection of editing tools";
+//        questions[6][2] = "A package is a collection of Classes";
+//        questions[6][3] = "A package is a collection of Classes and interfaces";
+//        questions[6][4] = "A package is a collection of interfaces";
+//
+//        questions[7][0] = "compareTo() returns";
+//        questions[7][1] = "True";
+//        questions[7][2] = "False";
+//        questions[7][3] = "An int value";
+//        questions[7][4] = "None";
+//
+//        questions[8][0] = "To which of the following does the class string belong to.";
+//        questions[8][1] = "java.lang";
+//        questions[8][2] = "java.awt";
+//        questions[8][3] = "java.applet";
+//        questions[8][4] = "java.String";
+//
+//        questions[9][0] = "Total constructor string class have.?";
+//        questions[9][1] = "3";
+//        questions[9][2] = "7";
+//        questions[9][3] = "13";
+//        questions[9][4] = "20";
+//
+//        answers[0][1] = "8";
+//        answers[1][1] = "32 and 64";
+//        answers[2][1] = "Int to Long";
+//        answers[3][1] = "The reference of the array";
+//        answers[4][1] = "Objects";
+//        answers[5][1] = "At rum time";
+//        answers[6][1] = "A package is a collection of Classes and interfaces";
+//        answers[7][1] = "An int value";
+//        answers[8][1] = "java.lang";
+//        answers[9][1] = "13";
 
         opt1 = new JRadioButton();
         opt1.setBounds(140,450,700,30);
@@ -206,16 +206,16 @@ public class Quiz extends JFrame implements ActionListener {
             opt4.setEnabled(true);
             ans_given = 1;
             if (group.getSelection() == null){
-                useranswers[count][0] = "";
+                Questions.useranswers[count][0] = "";
                 // Hangman funcions
                 HangmanUtils.updateImage();
                 // =======
             }else {
-                useranswers[count][0] = group.getSelection().getActionCommand();
+                Questions.useranswers[count][0] = group.getSelection().getActionCommand();
             }
 
-            for (int i=0; i<useranswers.length; i++){
-                if (useranswers[count][0].equals(answers[i][1])){
+            for (int i=0; i<Questions.useranswers.length; i++){
+                if (Questions.useranswers[count][0].equals(Questions.answers[i][1])){
                     score += 10;
                 }else {
                     score += 0;
@@ -241,15 +241,15 @@ public class Quiz extends JFrame implements ActionListener {
         } else if (e.getSource() == submit){
             ans_given = 1;
             if (group.getSelection() == null){
-                useranswers[count][0] = "";
+                Questions.useranswers[count][0] = "";
                 // Hangman funcion
                 HangmanUtils.updateImage();
                 // =======
             }else {
-                useranswers[count][0] = group.getSelection().getActionCommand();
+                Questions.useranswers[count][0] = group.getSelection().getActionCommand();
             }
-            for (int i=0; i<useranswers.length; i++){
-                if (useranswers[count][0].equals(answers[i][1])){
+            for (int i=0; i<Questions.useranswers.length; i++){
+                if (Questions.useranswers[count][0].equals(Questions.answers[i][1])){
                     score += 10;
                 }else {
                     score += 0;
@@ -297,13 +297,13 @@ public class Quiz extends JFrame implements ActionListener {
             }
             if (count == 9){
                 if (group.getSelection() == null){
-                    useranswers[count][0] = "";
+                    Questions.useranswers[count][0] = "";
                 }else {
-                    useranswers[count][0] = group.getSelection().getActionCommand();
+                    Questions.useranswers[count][0] = group.getSelection().getActionCommand();
                 }
 
-                for (int i=0; i<useranswers.length; i++){
-                    if (useranswers[i][0].equals(answers[i][1])){
+                for (int i=0; i<Questions.useranswers.length; i++){
+                    if (Questions.useranswers[i][0].equals(Questions.answers[i][1])){
                         score += 10;
                     }else {
                         score += 0;
@@ -315,9 +315,9 @@ public class Quiz extends JFrame implements ActionListener {
 
             } else {
                 if (group.getSelection() == null){
-                    useranswers[count][0] = "";
+                    Questions.useranswers[count][0] = "";
                 }else {
-                    useranswers[count][0] = group.getSelection().getActionCommand();
+                    Questions.useranswers[count][0] = group.getSelection().getActionCommand();
                 }
                 count++;
                 start(count);
@@ -327,19 +327,19 @@ public class Quiz extends JFrame implements ActionListener {
 
     public void start(int count){
         qno.setText(" "+(count+1)+". ");
-        question.setText(questions[count][0]);
+        question.setText(Questions.questions[count][0]);
 
-        opt1.setText(questions[count][1]);
-        opt1.setActionCommand(questions[count][1]);
+        opt1.setText(Questions.questions[count][1]);
+        opt1.setActionCommand(Questions.questions[count][1]);
 
-        opt2.setText(questions[count][2]);
-        opt2.setActionCommand(questions[count][2]);
+        opt2.setText(Questions.questions[count][2]);
+        opt2.setActionCommand(Questions.questions[count][2]);
 
-        opt3.setText(questions[count][3]);
-        opt3.setActionCommand(questions[count][3]);
+        opt3.setText(Questions.questions[count][3]);
+        opt3.setActionCommand(Questions.questions[count][3]);
 
-        opt4.setText(questions[count][4]);
-        opt4.setActionCommand(questions[count][4]);
+        opt4.setText(Questions.questions[count][4]);
+        opt4.setActionCommand(Questions.questions[count][4]);
 
         group.clearSelection();
     }
